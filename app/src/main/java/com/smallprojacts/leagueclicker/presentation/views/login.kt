2c9713d01e971f81de87fb39d060178fa241cce4
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-09-15 14:24:29 +0200

    testing nav

--- app/src/main/java/com/smallprojacts/leagueclicker/presentation/views/login.kt
+++ app/src/main/java/com/smallprojacts/leagueclicker/presentation/views/login.kt
@@ -15,12 +15,13 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.smallprojacts.leagueclicker.R
 import com.smallprojacts.leagueclicker.presentation.components.CustomTextField
 import com.smallprojacts.leagueclicker.presentation.components.ImageWithLogo
 
 @Composable
-fun LoginView() {
+fun LoginView(navController: NavHostController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -61,7 +62,9 @@ fun LoginView() {
                 .padding(horizontal = 20.dp, vertical = 40.dp)
         ) {
             Button(
-                onClick = {},
+                onClick = {
+                    navController.navigate("login_view")
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(65.dp)
@@ -78,12 +81,12 @@ fun LoginView() {
                     ),
                 shape = RoundedCornerShape(8.dp), // Ensure the shape matches the background
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Transparent // Make the button's container color transparent
+                    containerColor = Color.Transparent
                 ),
                 contentPadding = PaddingValues(0.dp) // Remove default padding
             ) {
                 Text(
-                    text = "dwadawd",
+                    text = "Login In",
                     color = Color(0xffF3F2F3),
                     fontSize = 20.sp
                 )
