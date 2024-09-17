48db9c0fd1165b909128b6cc7f73bd6d7ee209e5
abdelwahab farhat <abdelwahab.dev@gmail.com>
2024-09-17 16:55:00 +0200

    for validation

---
+++ app/src/main/java/com/smallprojacts/leagueclicker/core/utils/helper.kt
@@ -0,0 +1,32 @@
+package com.smallprojacts.leagueclicker.core.utils
+
+
+//object RegexHelper {
+    // Regular expression for a valid email
+//    val EMAIL_REGEX = Regex(
+//        "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+//    )
+//
+//    // Regular expression for a valid username (alphanumeric, underscore, or dot, 3 to 16 characters)
+//    val USERNAME_REGEX = Regex(
+//        "^[a-zA-Z][a-zA-Z0-9_.]{2,15}$"
+//    )
+//
+//    // Regular expression for a valid password (minimum 8 characters, at least one uppercase letter, one lowercase letter, one digit, and one special character)
+//    val PASSWORD_REGEX = Regex(
+//        "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,}$"
+//    )
+//
+//    // Functions to validate
+//    fun isEmailValid(email: String): Boolean {
+//        return EMAIL_REGEX.matches(email)
+//    }
+//
+//    fun isUsernameValid(username: String): Boolean {
+//        return USERNAME_REGEX.matches(username)
+//    }
+//
+//    fun isPasswordValid(password: String): Boolean {
+//        return PASSWORD_REGEX.matches(password)
+//    }
+//}
