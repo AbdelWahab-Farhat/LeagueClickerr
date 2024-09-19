1cc1bfb24cd3958dc3210963fda0b532d25bc191
Abubaker Naji <abubaker.fg@gmail.com>
2024-09-19 10:02:22 +0200

    My Champ DETAILS ADDED STATE AND EVENT FROM API

--- app/src/main/java/com/smallprojacts/leagueclicker/presentation/views/my_champ_details/my_champ_details.kt
+++ app/src/main/java/com/smallprojacts/leagueclicker/presentation/views/my_champ_details/my_champ_details.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -18,6 +19,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -30,6 +32,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.smallprojacts.leagueclicker.R
+import com.smallprojacts.leagueclicker.data.api.AllMyEvents
+import com.smallprojacts.leagueclicker.data.api.Champ
 import com.smallprojacts.leagueclicker.presentation.components.AbilitySelector
 import com.smallprojacts.leagueclicker.presentation.components.ChampionDetailsPicture
 import com.smallprojacts.leagueclicker.presentation.components.CustomButton
@@ -38,7 +42,18 @@ import com.smallprojacts.leagueclicker.presentation.components.StatMeterGrid
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyChampScreen(modifier: Modifier = Modifier, navController: NavHostController) {
+fun MyChampScreen(
+    modifier: Modifier = Modifier, navController: NavHostController,
+    state: MyChampDetailsState,
+    onEvent: (MyChampDetailsEvent) -> Unit,
+    championId: Int
+) {
+    LaunchedEffect(key1 = Unit) {
+//        ondynamicEvent(AllMyEvents.MyChampDetailsStateEvent(data = onEvent )).run {
+//            onEvent(MyChampDetailsEvent.PageInit(championId))
+//        }
+        onEvent(MyChampDetailsEvent.PageInit(championId))
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -56,15 +71,26 @@ fun MyChampScreen(modifier: Modifier = Modifier, navController: NavHostControlle
             topBar = {
                 CustomChampTopBar()
             },
+            bottomBar = {
+                Box(
+                    modifier = modifier
+                        .background(Color(0xff0A1428))
+                        .padding(horizontal = 20.dp,).padding(top = 5.dp) .navigationBarsPadding()
+                ) {
+                    CustomButton(onClick = {}, title = "Play")
+                }
+            }
+
         ) { innerPadding ->
-            val stats = mapOf(
-                "Strength" to 1,
-                "Intelligence" to 5,
-                "Speed" to 3,
-                "Health" to 2,
-                "Attack Damage" to 4,
-                "Ability Power" to 1
+            val statList = mapOf(
+                "Strength" to (state.champ.stats?.strength ?: 1),
+                "Intelligence" to (state.champ.stats?.intelligence ?: 1),
+                "Speed" to (state.champ.stats?.speed ?: 1),
+                "Health" to (state.champ.stats?.health ?: 1),
+                "Attack Damage" to (state.champ.stats?.attackDamage ?: 1),
+                "Attack Power" to(state.champ.stats?.abilityPower ?: 1)
             )
+
 
             Column(
                 modifier = modifier
@@ -73,10 +99,12 @@ fun MyChampScreen(modifier: Modifier = Modifier, navController: NavHostControlle
 
                 ChampionDetailsPicture(
                     modifier = modifier,
-                    "Ezreal",
-                    10,
-                    null,
-                    30000
+                   imagepath   = "${state.champ.championImage}",
+
+                    name = "${state.champ.name}",
+                    level = state.champ.stats?.strength?.plus(state.champ.stats?.health ?: 0)  ?: 0,
+                    title = null,
+                    numOfClicks = state.champ.numberOfClicks,
                 )
 
                 Column(
@@ -93,7 +121,7 @@ fun MyChampScreen(modifier: Modifier = Modifier, navController: NavHostControlle
 
                     Spacer(modifier = modifier.size(10.dp))
 
-                    StatMeterGrid(statMap = stats)
+                    StatMeterGrid(statMap = statList,state,onEvent)
 
                     Spacer(modifier = modifier.size(30.dp))
 
@@ -105,12 +133,15 @@ fun MyChampScreen(modifier: Modifier = Modifier, navController: NavHostControlle
                     )
 
                 }
-                AbilitySelector(modifier = modifier)
+
+               AbilitySelector(modifier = modifier,state, onEvent = onEvent)
+                Spacer(modifier =  Modifier.navigationBarsPadding().height(60.dp))
 
                 Box(modifier = modifier.padding(20.dp)) {
-                    CustomButton(onClick = {}, title = "Play")
+
                 }
             }
         }
     }
 }
+
