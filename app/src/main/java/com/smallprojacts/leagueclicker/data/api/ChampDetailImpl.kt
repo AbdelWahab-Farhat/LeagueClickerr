b8a74999a6a428276c004ec4347657a88828fba6
Abubaker Naji <abubaker.fg@gmail.com>
2024-09-19 10:15:39 +0200

    IMPLS FOR REPOS

---
+++ app/src/main/java/com/smallprojacts/leagueclicker/data/api/ChampDetailImpl.kt
@@ -0,0 +1,43 @@
+package com.smallprojacts.leagueclicker.data.api
+
+import android.util.Log
+import com.smallprojacts.leagueclicker.domain.models.AllChamp
+import com.smallprojacts.leagueclicker.domain.models.MyChamp
+import com.smallprojacts.leagueclicker.domain.models.repositories.ChampDetailsRepository
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.client.utils.EmptyContent.headers
+
+class ChampDetailImpl : ChampDetailsRepository {
+    override val client = ClientConfig.client
+    override suspend fun getChampDetails(champId: Int): AllChamp {
+        return try {
+            val test = client.get("${RemoteRoutes.GETDETAILCHAMP}$champId");
+            Log.d("tester", "${test.body<AllChamp>()}")
+            test.body<AllChamp>()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            AllChamp()
+        }
+    }
+
+    override suspend fun getMyChampDetails(): List<MyChamp> {
+        return try {
+            client.get("${RemoteRoutes.GETMYDETAILCHAMP}") {
+                headers {
+                    //  append("Authorization", "Bearer ${TokenManager.getToken()}")
+                    append(
+                        "Authorization",
+                        "Bearer 1|9yfUlOJYbDxZUSKHE27ZwLTq403PGQ0ufdYpYKO7a619c408"
+                    )
+                }
+            }.body<List<MyChamp>>()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptyList()
+        }
+    }
+
+}
+
